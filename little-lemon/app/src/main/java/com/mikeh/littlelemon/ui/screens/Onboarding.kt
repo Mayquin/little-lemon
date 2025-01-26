76a645357fe37da101af1d40d09d280ceac70781
Maycon Henrique <maycon.sousa.tavares@sibs.com>
2025-01-26 15:00:04 +0000

    Update onboarding top app bar

--- little-lemon/app/src/main/java/com/mikeh/littlelemon/ui/screens/Onboarding.kt
+++ little-lemon/app/src/main/java/com/mikeh/littlelemon/ui/screens/Onboarding.kt
@@ -1,45 +1,26 @@
 package com.mikeh.littlelemon.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.mikeh.littlelemon.R
 import com.mikeh.littlelemon.ui.components.BaseButton
 import com.mikeh.littlelemon.ui.components.BaseTextField
+import com.mikeh.littlelemon.ui.components.CustomTopAppBar
 import com.mikeh.littlelemon.ui.theme.HighlightWhite
 import com.mikeh.littlelemon.ui.theme.PrimaryGreen
 
@@ -50,18 +31,7 @@ import com.mikeh.littlelemon.ui.theme.PrimaryGreen
 
 @Composable
 fun Onboarding() {
-    Scaffold(topBar = {
-        Row(
-            Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Image(
-                modifier = Modifier.size(width = 150.dp, height = 50.dp),
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "logo"
-            )
-        }
-    }) { paddingValues ->
+    Scaffold(topBar = { CustomTopAppBar() }) { paddingValues ->
         val name = remember {
             mutableStateOf("")
         }
