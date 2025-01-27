1ee56c43093d3fcc1aeecff18541a3ca52711593
Maycon Henrique <maycon.sousa.tavares@sibs.com>
2025-01-27 21:19:06 +0000

    Setup fetch content and app database

--- little-lemon/app/src/main/java/com/mikeh/littlelemon/MainActivity.kt
+++ little-lemon/app/src/main/java/com/mikeh/littlelemon/MainActivity.kt
@@ -3,18 +3,33 @@ package com.mikeh.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.mikeh.littlelemon.api.model.AppDatabase
+import com.mikeh.littlelemon.api.model.MenuItemNetwork
+import com.mikeh.littlelemon.api.model.MenuNetworkData
 import com.mikeh.littlelemon.ui.screens.navigation.Navigation
 import com.mikeh.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,5 +37,18 @@ class MainActivity : ComponentActivity() {
                 Navigation(navController = rememberNavController())
             }
         }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val menuContentUrl = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        return httpClient
+            .get(urlString = menuContentUrl)
+            .body<MenuNetworkData>()
+            .menu
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 }
---
+++ little-lemon/app/src/main/java/com/mikeh/littlelemon/api/model/Network.kt
@@ -0,0 +1,34 @@
+package com.mikeh.littlelemon.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Created by Maycon Henrique on 26/01/2025.
+ * maycon255@hotmail.com
+ */
+
+@Serializable
+data class MenuItemNetwork(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("price")
+    val price: Double,
+    @SerialName("category")
+    val category: String,
+) {
+    fun toMenuItemRoom() = MenuItemRoom(
+        id,
+        title,
+        price,
+        category
+    )
+}
+
+@Serializable
+data class MenuNetworkData(
+    @SerialName("menu")
+    val menu: List<MenuItemNetwork>
+)
