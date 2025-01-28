f3cb39c17106b22531a1e9760521b4bab3075aa3
Maycon Henrique <maycon.sousa.tavares@sibs.com>
2025-01-28 01:01:30 +0000

    Update button component

--- little-lemon/app/src/main/java/com/mikeh/littlelemon/ui/components/Buttons.kt
+++ little-lemon/app/src/main/java/com/mikeh/littlelemon/ui/components/Buttons.kt
@@ -2,10 +2,14 @@ package com.mikeh.littlelemon.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 
 /**
@@ -15,9 +19,12 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun BaseButton(btnTitle: String, onClick: () -> Unit){
-    Button( modifier = Modifier.fillMaxWidth(),
-        onClick = onClick) {
-        Text(text = btnTitle)
+    Button(modifier = Modifier.fillMaxWidth(),
+        onClick = onClick,
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer)) {
+        Text(text = btnTitle, fontWeight = FontWeight.Bold, fontSize = 16.sp)
     }
 }
 
